9069188a5c2e9020068a17d0e10cce1c4440b696
TI4C <TI4C@TI4C-PC>
2022-08-19 13:58:42 -0300

    Adicionar arquivos de projeto.

---
+++ Blog/Controller/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controller {
+    [ApiController]
+    public class CategoryController : ControllerBase {
+        private readonly BlogDataContext _context;
+        public CategoryController(BlogDataContext context) {
+            _context = context;
+        }
+
+        [HttpGet("categories")]
+        public IActionResult Get() => Ok(_context.Categories.ToList());
+
+    }
+}
---
+++ Blog/Program.cs
@@ -0,0 +1,12 @@
+using Blog.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddControllers();  
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
