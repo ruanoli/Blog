64bfa95d6bdd8a9c1b575a9550a468d1ec1bdd26
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2022-09-10 17:21:50 -0300

    jwt

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,14 +1,40 @@
+using Blog;
 using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Configuração da autenticação
+var key = Encoding.ASCII.GetBytes(Configuration.JwtToken);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>();
+
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options => 
     { options.SuppressModelStateInvalidFilter = true; });
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
---
+++ Blog/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Blog.Services
+{
+    public class TokenService
+    {
+        //Gerando token pro usuário token
+        public string GenerateToken(User user)
+        {
+            //manipulador de token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            //chave
+            //O tokenHandler espera um array de bytes e não uma string.
+            //convertendo a chave para bytes:
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtToken);
+
+            //configurações do token, contém todas as informações do token.
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,"ruanoliveira"), //User.Identity.Name
+                    new Claim(ClaimTypes.Role,"admin"), //User.IsInRole
+                    new Claim("fruta", "banana")
+                }),
+                //Definir o tempo de duração do token
+                Expires = DateTime.UtcNow.AddHours(8),
+
+                //como token é gerado e lido posteriormente. Encriptando e desencriptando.
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+
+            };
+
+            //gerando o token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            
+            //WriteToken retorna uma string
+            return tokenHandler.WriteToken(token); 
+        }
+    }
+}
